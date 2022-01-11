17ca0458095a979d1c2344979146cfb562055c0b
Mateus Seixas <mateusinhokarambi@hotmail.com>
2022-01-11 16:00:52 -0300

    #10 Creating administrator database seed

---
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -0,0 +1,20 @@
+import { hash } from 'bcrypt';
+import { createConnection } from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
+
+async function create() {
+  const connection = await createConnection();
+
+  const id = uuidv4();
+  const hashedPassword = await hash('admin', 8);
+
+  await connection.query(`
+    INSERT INTO users VALUES (
+      '${id}', 'Administrator', 'admin@hublocal.com', '${hashedPassword}', true, 'now()', 'now()'
+    )
+  `);
+
+  await connection.close();
+}
+
+create().then(() => console.log('Admin user created'));
