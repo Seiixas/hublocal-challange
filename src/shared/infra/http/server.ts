bfe4ab992350511b1eb1cc7ee672c091e3bcd59b
Mateus Seixas <mateusinhokarambi@hotmail.com>
2022-01-14 13:54:41 -0300

    #25 Creating the API documentation using Swagger

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,16 +1,21 @@
 import 'reflect-metadata';
-import 'express-async-errors';
 import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 
 import '../typeorm';
 import '../../container';
 
+import swaggerUI from 'swagger-ui-express';
+
+import swaggerFile from '../../../swagger.json';
 import { AppError } from '../../errors';
 import { routes } from './routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use(routes);
 
